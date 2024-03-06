b72dfa3db056f9a2ce9c5667745f45d5d386ea29
Oleksandr Putsyk <sashaputsyk100903@gmail.com>
2024-03-06 23:18:16 +0200

    Add txt file dz

---
+++ WebRozetka/WebRozetka/Data/SeederDB.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebRozetka.Constants;
+using WebRozetka.Data.Entities;
+using WebRozetka.Data.Entities.Identity;
+using WebRozetka.Helpers;
+using WebRozetka.Interfaces;
+
+namespace WebRozetka.Data
+{
+    public static class SeederDB
+    {
+        public static void SeedData(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var service = scope.ServiceProvider;
+                //Отримую посилання на наш контекст
+                var context = service.GetRequiredService<AppEFContext>();
+                context.Database.Migrate();
+
+                var userManager = scope.ServiceProvider
+                    .GetRequiredService<UserManager<UserEntity>>();
+
+                var roleManager = scope.ServiceProvider
+                    .GetRequiredService<RoleManager<RoleEntity>>();
+
+                var novaPoshta = scope.ServiceProvider.GetRequiredService<INovaPoshtaService>();
+                if (!context.Areas.Any())
+                {
+                    novaPoshta.GetAreas();
+                }
+
+                if (!context.Settlements.Any())
+                {
+                    novaPoshta.GetSettlements();
+                }
+
+                if (!context.Warehouses.Any())
+                {
+                    novaPoshta.GetWarehouses();
+                }
+
+
+                #region Додавання користувачів та ролей
+
+                if (!context.Roles.Any())
+                {
+                    foreach (var role in Roles.All)
+                    {
+                        var result = roleManager.CreateAsync(new RoleEntity
+                        {
+                            Name = role
+                        }).Result;
+                    }
+                }
+
+                if (!context.Users.Any())
+                {
+                    var user = new UserEntity
+                    {
+                        FirstName = "Павло",
+                        LastName = "Марко",
+                        Email = "admin@gmail.com",
+                        UserName = "admin@gmail.com"
+                    };
+                    var result = userManager.CreateAsync(user, "123456").Result;
+                    if (result.Succeeded)
+                    {
+                        result = userManager.AddToRoleAsync(user, Roles.Admin).Result;
+                    }
+                }
+
+                #endregion
+            }
+        }
+    }
+}
